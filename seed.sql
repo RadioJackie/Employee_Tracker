b54d212b1dee2e5fa18e1c6e6e2991a2f3712cab
RadioJackie <jacobpeters628@gmail.com>
2020-06-24 00:54:38 -0700

    Made and finished the sql file and started the index js file also installed all the json.

---
+++ seed.sql
@@ -0,0 +1,42 @@
+DROP DATABASE IF EXISTS employee_trackerDB
+
+CREATE DATABASE employee_trackerDB
+
+USE employee_trackerDB
+
+CREATE TABLE employee(
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR (30) NOT NULL,
+last_name VARCHAR (30) NOT NULL,
+role_id INT,
+PRIMARY KEY (id)
+)
+
+INSERT INTO employee 
+(first_name, last_name, role_id, manager_id)
+VALUES 
+('Billy','Crystal',1, NULL),
+('Eddie','Murphy',2, 1),
+('Richard','Pryor',3, NULL),
+('George','Carlin',4, 3),
+('Joan','Rivers',5, NULL),
+('Robin','Williams',6, 5),
+('Bill','Burr',7, NULL),
+('Louie','CK',8, 7)
+
+CREATE TABLE employee_role(
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL,
+department_id INT,
+PRIMARY KEY (id),
+)
+INSERT INTO employee_role (title, salary, department_id) values ('Sales Lead', 100000, 1);
+INSERT INTO employee_role (title, salary, department_id) values ('Sales Person', 80000, 1);
+INSERT INTO employee_role (title, salary, department_id) values ('Lead Engineer', 200000, 2);
+INSERT INTO employee_role (title, salary, department_id) values ('Software Engineer', 150000, 2);
+INSERT INTO employee_role (title, salary, department_id) values ('Accounting Lead', 170000, 3);
+INSERT INTO employee_role (title, salary, department_id) values ('Accountant', 125000, 3);
+INSERT INTO employee_role (title, salary, department_id) values ('Legal Team Lead', 150000, 4);
+INSERT INTO employee_role (title, salary, department_id) values ('Lawyer', 125000, 4);
+
