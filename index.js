c0e82324ec5823ef8398d0a8b9b4ec097afec545
RadioJackie <jacobpeters628@gmail.com>
2020-06-24 01:13:46 -0700

    made a function and it's late

--- index.js
+++ index.js
@@ -9,3 +9,33 @@ const connection = mysql.createConnection({
     password: "password",
     database: "employee_tracker"
 });
+
+connection.connect(function (err) {
+    if (err) throw err;
+
+    console.log("connection id", connection.threadId);
+    displayMenu()
+});
+
+function displayMenu() {
+    inquirer.prompt({
+        name: "choice",
+        type: "list",
+        message: "What would you like to to?",
+        choices: [
+            "View All Employees",
+            "View All Employees by Department",
+            "View All Employees by Manager",
+            "Add Employee",
+            "Remove Employee",
+            "Update Employee Role",
+            "Update Employee Manager",
+            "View All Roles",
+            "Add Role",
+            "Remove Role",
+            "View All Department",
+            "Add Department",
+            "Remove Department",
+            "Quit"
+        ]
+        
